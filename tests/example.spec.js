daccfa662818ea61e9dbefae919af9cce0769513
monika <monika-nowakowska@outlook.com>
2026-03-15 15:38:34 +0100

    github workflows added

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://mad-qa.pl/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
